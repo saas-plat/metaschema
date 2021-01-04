56c7f84920a8093695528b23f6ef0afbfb3124ea
caizx <caizx@chanjet.com>
2021-01-04 15:37:42 +0800

    refactor: bundle还是需要的，只是不需要手动编写，自动生成代码
    Revert "feat: bundle 被取消"
    
    This reverts commit 32c40e8e03cc2e14200569b8a51ae3cb9d21a703.

--- test/Bundle.test.js
+++ test/Bundle.test.js
@@ -1,14 +1,11 @@
-const {
-  expect
-} = require('chai');
-const {
-  Bundle,
-  ViewModel
-} = require('../lib');
+const { expect } = require('chai');
+const { Bundle, ViewModel } = require('../lib');
 
 describe('打包', () => {
-
   it('建立对象规则关系', () => {
-    Bundle('test aa', ViewModel('aa', {}))
-  })
-})
+    const b = Bundle('test aa', { a: ViewModel('aa', {}) });
+    const m = b.getModel('aa');
+    expect(m).to.not.null;
+    expect(b.getModel('a')).to.be.undefined;
+  });
+});
